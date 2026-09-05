070adc618dbd0aad8a84ce4f6c86758ca67e2540
SzylekPL <michalchrobak06@gmail.com>
2026-09-05 13:58:36 +0200

    added primary constructor analysis

--- Generator/MainAnalyzer.cs
+++ Generator/MainAnalyzer.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using OrmGenerator.Utility;
+using Shared;
 using System.Collections.Immutable;
 using System.Linq;
 using static Shared.ProjectDiagnostics;
@@ -22,9 +24,7 @@ public class MainAnalyzer : DiagnosticAnalyzer
 		{
 			INamedTypeSymbol type = (INamedTypeSymbol)ctx.Symbol;
 
-			AttributeData? attribute = type
-				.GetAttributes()
-				.FirstOrDefault(static attr => attr.AttributeClass?.Name == "OrmModelAttribute");
+			AttributeData? attribute = GetModelAttribute(type);
 
 			if (attribute is null) return;
 
@@ -37,28 +37,59 @@ public class MainAnalyzer : DiagnosticAnalyzer
 					.SelectMany(r => r.GetSyntax(ctx.CancellationToken).ChildNodes())
 					.FirstOrDefault(n => n is ParameterListSyntax);
 
-				if (paramList is not { Parameters.Count: > 0 })
+				if (paramList is null)
 				{
 					ctx.ReportDiagnostic(Diagnostic.Create(_ctorNotSuitableRule, type.Locations[0], type.Name));
 					return;
 				}
-
-				//todo: diagnostics report when trying to use non-models in mapping
 			}
 		}, SymbolKind.NamedType);
+
+		context.RegisterSyntaxNodeAction(static ctx =>
+		{
+			if (ctx.ContainingSymbol is not INamedTypeSymbol type || GetModelAttribute(type) is not AttributeData attribute)
+				return;
+
+			if (!((ModelOptions)attribute.ConstructorArguments[0].Value!).HasFlag(ModelOptions.UsePrimaryConstructor) && !type.IsRecord)
+				return;
+
+			ParameterListSyntax paramList = (ParameterListSyntax)ctx.Node;
+
+			if (paramList.Parameters.Count == 0)
+			{
+				ctx.ReportDiagnostic(Diagnostic.Create(_ctorNotSuitableRule, type.Locations[0], type.Name));
+				return;
+			}
+
+			foreach (ParameterSyntax param in paramList.Parameters)
+			{
+				IParameterSymbol paramSymbol = ctx.SemanticModel.GetDeclaredSymbol(param, ctx.CancellationToken)!;
+
+				if (!Constants.DbDataTypes.Contains(paramSymbol.Type.Name) && !HasModelAttribute(paramSymbol.Type))
+					ctx.ReportDiagnostic(Diagnostic.Create(_notMarkedRule, param.GetLocation(), paramSymbol.Type.Name, paramSymbol.Name));
+			}
+
+		}, SyntaxKind.ParameterList);
 
 		context.RegisterSymbolAction(static ctx =>
 		{
 			IPropertySymbol prop = (IPropertySymbol)ctx.Symbol;
 
-			if (!prop.ContainingType.GetAttributes().Any(static a => a.AttributeClass?.Name is "OrmModelAttribute"))
+			if (!HasModelAttribute(prop.ContainingType))
 				return;
 
-			if (prop.Type.GetAttributes().Any(static a => a.AttributeClass?.Name is "OrmModelAttribute"))
+			if (HasModelAttribute(prop.Type))
 				return;
 
 			ctx.ReportDiagnostic(Diagnostic.Create(_notMarkedRule, prop.Locations[0], prop.Type.Name, prop.Name));
 
 		}, SymbolKind.Property);
 	}
+
+	private static AttributeData? GetModelAttribute(ITypeSymbol type) => type
+		.GetAttributes()
+		.FirstOrDefault(static attr => attr.AttributeClass?.Name == "OrmModelAttribute");
+	private static bool HasModelAttribute(ITypeSymbol type) => type
+		.GetAttributes()
+		.Any(static a => a.AttributeClass?.Name == "OrmModelAttribute");
 }
